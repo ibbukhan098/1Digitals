4367c2356ad4658645d451354b283e282240c0c0
Rajashekar Reddy <rajashekar@grabon.in>
2023-01-23 14:44:31 +0530

    HomePage changes

---
+++ public/js/customs.js
@@ -0,0 +1,146 @@
+let primaryMenu = $('.primary-menu'),
+    $body1 = $('body'),
+    menuSpeed = primaryMenu.attr( 'data-trigger-speed' ) || 200,
+    meuContainer = $('.primary-menu .menu-container'),
+    menuSocials = $('.primary-menu .menu-socials');
+menuSpeed = Number(menuSpeed);   
+let clicked = false;
+let navItems = [...meuContainer[0].children].filter((ele)=>{
+    return ele.classList.contains('menu-item')
+})
+$('#custom-primary-menu-trigger').off( 'click' ).on( 'click', function() {
+    // primaryMenu.attr('data-state') == 'closed'? primaryMenu.attr('data-state','opened') : primaryMenu.attr('data-state','closed');
+    // isOpened = !isOpened;
+    if(clicked){
+        return;
+    }
+    
+    clicked = true;
+    const isOpened = primaryMenu.attr('aria-expanded');
+
+    if(isOpened == "false"){
+        openedNavPopup();
+        
+    }
+    else{
+        closingNavPopup();
+        
+    }
+    
+    
+    if( $body1.hasClass('device-md') || $body1.hasClass('device-sm') || $body1.hasClass('device-xs') ) {
+        if( primaryMenu.find('.mobile-primary-menu').length > 0 ) {
+            $( '.primary-menu:not(.mobile-menu-off-canvas) .mobile-primary-menu' ).stop( true, true ).slideToggle( menuSpeed );
+            $( '.primary-menu.mobile-menu-off-canvas .mobile-primary-menu' ).toggleClass('d-block');
+        } else {
+            $( '.primary-menu:not(.mobile-menu-off-canvas) .menu-container' ).stop( true, true ).slideToggle( menuSpeed );
+            $( '.primary-menu.mobile-menu-off-canvas .menu-container' ).toggleClass('d-block');
+        }
+    }
+    clicked = false;
+});
+
+
+
+function openedNavPopup(){
+    primaryMenu.attr('aria-expanded','true');
+    $body1.addClass("primary-menu-open");
+    primaryMenu.attr('data-state','opened');
+
+    let openedEle = document.querySelector('.primary-menu[data-state="opened"] .primary-row-overlay');
+    if(openedEle != null){
+        openedEle.addEventListener('animationend',()=>{
+            meuContainer.css({
+                'opacity':'1'
+            })
+            
+
+            navItems.forEach(async(ele,index)=>{
+                ele.classList.remove('backOutLeft','animated');
+                ele.classList.add('backInRight','animated');
+            })
+        },{once:true})
+    }
+
+}
+
+function closingNavPopup(){
+    primaryMenu.attr('aria-expanded','false');
+    primaryMenu.attr('data-state','closing');
+
+    let closingEle = document.querySelector('.primary-menu[data-state="closing"] .primary-row-overlay');
+    if(closingEle != null){
+        closingEle.addEventListener('animationstart',()=>{
+            const navAnim = new Promise((resolve,reject)=>{
+                // debugger;
+                navItems.forEach((ele,index)=>{
+                    ele.classList.remove('backInRight', 'animated');
+                    ele.classList.add('backOutLeft', 'animated');
+                })
+                resolve();
+                
+            })
+            navAnim.then((res)=>{
+                // debugger;
+                meuContainer.css({
+                    'opacity':'0',
+                    'animation-duration':'350ms !important'
+                });
+            })
+            
+        },{once:true})
+        closingEle.addEventListener('animationend',()=>{
+            closedNavPopup();
+        },{once:true})
+    }
+}
+
+function closedNavPopup(){
+    $body1.removeClass("primary-menu-open");
+    primaryMenu.attr('data-state','closed');    
+}
+
+
+
+
+
+/* Tab Functionality */
+
+let btnList = document.querySelectorAll('.section-tabs .btn-list .list-btn');
+let effectImg = document.querySelector('.click-effect img');
+
+if(btnList != null){
+    btnList.forEach((btn,index)=>{
+        // if(!(btn.classList.contains('active'))){
+
+            btn.addEventListener('click',()=>{
+                let cls =[...btnList].filter((ele)=>{
+                    return ele.classList.contains('active');
+                });
+                cls[0].classList.remove('active');
+                btn.classList.add('active');
+                if(effectImg != null){
+                    let boom = new Promise((resolve,reject)=>{
+                        effectImg.classList.remove('backInUp','animated');
+                        resolve();
+                    })
+                    boom.then(()=>{
+                        setTimeout(()=>{
+                            effectImg.classList.add('backOutDown','animated');
+                        },10)
+                    }).then(()=>{
+                        setTimeout(()=>{
+                            effectImg.classList.remove('backOutDown','animated');
+                        },10)
+                    }).then(()=>{
+                        setTimeout(()=>{
+                            effectImg.classList.add('backInUp','animated');
+                        },10)
+                    }).then(()=>{
+                        effectImg.setAttribute('src',`images/services/${btn.getAttribute('data-ptype')}-portal.png`);
+                    })
+                }
+            })
+        // }
+    })
+}
