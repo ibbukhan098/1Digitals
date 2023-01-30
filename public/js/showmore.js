566c676abf813456d8238087409a875073e7a7da
Rajashekar Reddy <rajashekar@grabon.in>
2023-01-30 19:14:35 +0530

    Review changes

--- public/js/showmore.js
+++ public/js/showmore.js
@@ -5,27 +5,28 @@ export default function ShowMore(ele){
 
 
     showMore.forEach((showmore,index)=>{
-        let isshowmore = isShowmore(showmore);
+        let ismaxheightreduce = showmore.classList.contains('mx-h-3');
+        let isshowmore = isShowmore(showmore,ismaxheightreduce);
 
         
         if(isshowmore.result){
             // showMore.innerText 
-            showmore.innerHTML += `<button class="showmore-btn">...Show <span>More</span></button>`;
+            showmore.innerHTML += `<button class="showmore-btn"><span>...Show More</span></button>`;
             
         }
 
-        let value = isshowmore.pValue;
+        let value = ismaxheightreduce ? isshowmore.pValue-20 : isshowmore.pValue;
         showmore.querySelector('.showmore-btn').addEventListener('click',(e)=>{
             let spanEle = [...e.target.children];
             if(e.target.classList.contains('active')){
                 showmore.style.maxHeight = `${value}px`;
                 e.target.classList.remove('active');
-                spanEle[0].innerHTML = 'More'
+                spanEle[0].innerHTML = '...Show More'
             }
             else{
                 showmore.style.maxHeight = '100%';
                 e.target.classList.add('active');
-                spanEle[0].innerHTML = 'Less'
+                spanEle[0].innerHTML = 'Show Less'
             }
         })
 
@@ -36,9 +37,9 @@ export default function ShowMore(ele){
 
 }
 
-function isShowmore(ele){
+function isShowmore(ele,ismaxheightreduce=false){
     let pValue = ele.parentElement.clientHeight;
-    ele.style.maxHeight = `${pValue}px`
+    ele.style.maxHeight = `${ismaxheightreduce ? pValue-20 : pValue}px`
     let clientHeight = ele.clientHeight,
         scrollHeight = ele.scrollHeight,
         result = scrollHeight > clientHeight;
