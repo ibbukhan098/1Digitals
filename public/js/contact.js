a5841c7ae853eef85daa6921f935d222d92bb43f
Rajashekar Reddy <rajashekar@grabon.in>
2023-02-08 14:58:34 +0530

    Live changes

---
+++ public/js/contact.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+
+    $('.formContact').submit(function (e) {
+        e.preventDefault();
+        var formIndex = $(this).attr('data-formIndex');
+        var fname = $(this).find('.fname').val(),
+            email = $(this).find('.email').val(),
+            subject = $(this).find('.subject').val(),
+            userMessage = $(this).find('.message').val();
+
+
+        if (fname == '' || email == '' || subject == "" || userMessage == "") {
+            $('.statusForm').fadeIn(500).html('All fields are mandatory');
+        }
+
+        else {
+            Email.send({
+                Host: "smtp.elasticemail.com",
+                Username: "anereddyshanreddy@gmail.com",
+                Password: "E58DAA3A46B771930C78829C43EADCF04DFB",
+                To: "anereddyshanreddy@gmail.com",
+                From: 'anereddyshanreddy@gmail.com',
+                Subject: `Query From ${fname}`,
+                Body: `<html>
+                        <div>
+                        <h2>User Details</h2>
+                            <table style="width:500px;border:1px solid #000">
+                                <tr style="border:1px solid #000; padding:10px 0px;background-color:#000;color:#fff">
+                                    <th style="padding:5px">Name</th>
+                                    <th style="padding:5px">Email</th>
+                                    <th style="padding:5px">Subject</th>
+                                    <th style="padding:5px">Message</th>
+                                </tr>
+
+                                <tr style="border:1px solid #000">
+                                    <td style="padding:5px">${fname}</td>
+                                    <td style="padding:5px">${email}</td>
+                                    <td style="padding:5px">${subject}</td>
+                                    <td style="padding:5px">${userMessage}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </html>       
+                `,
+            })
+                .then(function (message) {
+                    console.log(message)
+                    $('.form-process').css({'display':'none'})
+                    $('.statusForm').css({
+                        'color': 'green'
+                    })
+                    $('.statusForm').fadeIn(500).html('The form was submitted successfully!');
+                    $('.fname').val('');
+                    $('.email').val('');
+                    $('.subject').val('');
+                    $('.message').val('');
+                });
+
+
+                setTimeout(()=>{
+                    $('.statusForm').fadeOut().html('');
+                },5000)
+        }
+    });
+});
--- public/js/showmore.js
+++ public/js/showmore.js
@@ -6,7 +6,11 @@ export default function ShowMore(ele){
 
     showMore.forEach((showmore,index)=>{
         let ismaxheightreduce = showmore.classList.contains('mx-h-3');
-        let isshowmore = isShowmore(showmore,ismaxheightreduce);
+        let marginvalue = 0;
+        if(showmore.classList.contains('forabout')){
+            marginvalue = 70;
+        }
+        let isshowmore = isShowmore(showmore,ismaxheightreduce,marginvalue);
 
         
         if(isshowmore.result){
@@ -15,7 +19,7 @@ export default function ShowMore(ele){
             
         }
 
-        let value = ismaxheightreduce ? isshowmore.pValue-20 : isshowmore.pValue;
+        let value = ismaxheightreduce && marginvalue  == 0 ? isshowmore.pValue-20 : !ismaxheightreduce && marginvalue != 0 ? isshowmore.pValue-marginvalue : isshowmore.pValue;
         showmore.querySelector('.showmore-btn').addEventListener('click',(e)=>{
             let spanEle = [...e.target.children];
             if(e.target.classList.contains('active')){
@@ -37,9 +41,9 @@ export default function ShowMore(ele){
 
 }
 
-function isShowmore(ele,ismaxheightreduce=false){
+function isShowmore(ele,ismaxheightreduce=false,marginvalue=0){
     let pValue = ele.parentElement.clientHeight;
-    ele.style.maxHeight = `${ismaxheightreduce ? pValue-20 : pValue}px`
+    ele.style.maxHeight = `${ismaxheightreduce && marginvalue  == 0 ? pValue-20 : !ismaxheightreduce && marginvalue != 0 ? pValue-marginvalue : pValue}px`
     let clientHeight = ele.clientHeight,
         scrollHeight = ele.scrollHeight,
         result = scrollHeight > clientHeight;
